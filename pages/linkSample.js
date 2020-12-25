f9dc80ea2c4316ec010699e57945499a76c3e8de
pvcresin <pvcresin0730@gmail.com>
2020-12-25 22:41:46 +0900

    Update sample page

--- pages/linkSample.js
+++ pages/linkSample.js
@@ -1,14 +1,7 @@
 import Link from "next/link";
 
 const Index = () => {
-  return (
-    <>
-      <div>Welcome to Next.js!</div>
-      <Link href="/linkSample">
-        <a>link Sample Page</a>
-      </Link>
-    </>
-  );
+  return <div>This is the sample page</div>;
 };
 
 export default Index;
